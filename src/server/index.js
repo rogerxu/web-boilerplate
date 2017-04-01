d30b2b43f9e58060b258088734abff48df235b2a
Roger Xu <roger.xu.2007@gmail.com>
2017-04-01 21:56:16 +0800

    Add express

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,21 @@
 /* eslint-disable no-console */
-import Dog from '../shared/dog';
+import compression from 'compression';
+import express from 'express';
 
-const toby = new Dog('Toby');
-console.log(toby.bark());
+import { APP_NAME, STATIC_PATH, WEB_PORT } from '../shared/config';
+import { isProd } from '../shared/util';
+import renderApp from './render-app';
+
+const app = express();
+
+app.use(compression());
+app.use(STATIC_PATH, express.static('dist'));
+app.use(STATIC_PATH, express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send(renderApp(APP_NAME));
+});
+
+app.listen(WEB_PORT, () => {
+  console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' : '(development)'}.`);
+});
