103f5b78b589364de442b33a3a18f25cb9455f19
Roger Xu <roger.xu.2007@gmail.com>
2017-04-02 19:18:27 +0800

    Add react-router

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -7,6 +7,7 @@ import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './app';
 import helloReducer from './reducer/hello';
@@ -28,9 +29,11 @@ const rootEl = document.querySelector(APP_CONTAINER_SELECTOR);
 
 const wrapApp = (AppComponent, reduxStore) =>
   <Provider store={reduxStore}>
-    <AppContainer>
-      <AppComponent />
-    </AppContainer>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
   </Provider>;
 
 ReactDOM.render(wrapApp(App, store), rootEl);
